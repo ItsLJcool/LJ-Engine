609e7104e8538956dd7e6bd2b14975b20744e767
Landon Shields <lchammer0701@gmail.com>
2023-05-18 21:41:17 -0700

    thing

--- source/engine/PlayState.hx
+++ source/engine/PlayState.hx
@@ -2,15 +2,12 @@ package engine;
 
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	override public function create()
-	{
+class PlayState extends FlxState {
+	override public function create() {
 		super.create();
 	}
 
-	override public function update(elapsed:Float)
-	{
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
 	}
 }
